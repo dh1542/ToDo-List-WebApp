155e3c3359fd0ca70cef5424b7d6e881f571fd43
Dominik <Dominik@mbp>
2022-02-10 13:48:34 +0100

    added list items, added boilerplate JS

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// append button to end of list
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
