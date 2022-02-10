d379e3d22bfa0cb3fd52d95321db5a2cba4df2c3
dominik-arch <dominik.a.hoffmann@fau.de>
2022-02-10 10:31:30 -0500

    its now possible to add list items

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 
-// append button to end of list
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+console.log("file read");
+// get string out of textfield once add is pressed
+
+// add event-listener for button
+document.getElementById('button').addEventListener("click", function(){addNewListElement()});
+
+
+
+function printText(){
+  var input = document.getElementById('text-field').value;
+  console.log(input);
+}
+
+function addNewListElement(){
+  var input = document.getElementById('text-field').value;
+  
+  // warn if nothing is in text-field
+  if(input.length == 0){
+    alert("Insert a task!");
+    return;
   }
-}, false);
+  // else add list element
+  else{
+    document.getElementById('theList').innerHTML += ('<li>'+input+'</li>');
+  }
+}
+
 
